a13792f5cfdf0c75a80b01607e58b3eca1e822eb
Nana <nanakzy@gmail.com>
2024-03-20 00:28:11 +0200

    user.py

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -119,21 +119,31 @@ class HBNBCommand(cmd.Cmd):
             if not args:
                 print("** class name missing **")
                 return
+
             args = args.split()
             class_name = args[0]
             arg_list = {}
+
+            if class_name not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+
+            if len(args) == 1:
+                new_instance = HBNBCommand.classes[class_name]()
+                new_instance.save()
+                print(new_instance.id)
+                return
+
             for arg in args[1:]:
                 split_arg = arg.split("=")
                 split_arg[1] = eval(split_arg[1])
             if isinstance(split_arg[1], str):
                 split_arg[1] = split_arg[1].replace('_', '" "').replace('"', '\\"')
                 arg_list[split_arg[0]] = split_arg[1]
-            if class_name not in HBNBCommand.classes:
-                raise NameError("** class doesn't exist **")
 
-            new_instance = HBNBCommand.classes[arg_list[0]](**arg_list)
-            new_instanice.save()
-            print(new_instance.id)
+                new_instance = HBNBCommand.classes[class_name](**arg_list)
+                new_instance.save()
+                print(new_instance.id)
 
         except NameError as e:
             print(e)
@@ -284,7 +294,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -292,10 +302,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -331,6 +341,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            filtered_objs = {}
+            for key, value in self.__objects.items():
+                if type(value) == cls:
+                    filtered_objs[key] = value
+                    return filtered_objs
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -54,3 +61,7 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             del self.__objects[key]
+
+    def close(self):
+        """ reload function """
+        self.reload()
