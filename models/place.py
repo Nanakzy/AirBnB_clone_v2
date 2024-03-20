f767260db34f5649ad82f195e48c72c144f596f8
Miss-Siba <sibaquma@gmail.com>
2024-03-20 09:05:18 -0700

    place

--- models/place.py
+++ models/place.py
@@ -4,15 +4,14 @@ from models.base_model import BaseModel
 from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 import models
+import shlex
 from os import getenv
 from sqlalchemy.ext.declarative import declarative_base
 
 
 class Place(BaseModel):
     """ A place to stay """
-
     __tablename__ = 'places'
-
     name = Column(String(128), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
@@ -23,17 +22,41 @@ class Place(BaseModel):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    amenity_ids = []
 
-    reviews = relationship("Review", cascade="all, delete", backref="place")
-
-    @property
-    def reviews(self):
-        """getter attribute reviews that returns the list of Review instances
-           with place_id equals to the current Place.id
-        """
-        from models import storage
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade="all, delete",
+                               backref="place")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
+    else:
+        @property
+        def reviews(self):
+            """getter attribute reviews that returns the list of
+            Review instances
+            with place_id equals to the current Place.id
+            """
+        all_objects = models.storage.all()
         review_list = []
-        for review in storage.all(Review).values():
-            if review.place_id == self.id:
-                review_list.append(review)
-        return review_list
+        result = []
+        for key in all_objects:
+            review = key.replace('.', ' ')
+            review = shlex.split(review)
+            if review[0] == 'Review':
+                review_list.append(all_objects[key])
+        for elem in review_list:
+            if elem.place_id == self.id:
+                result.append(elem)
+        return result
+
+        @property
+        def amenities(self):
+            """ Returns list of amenity ids """
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ Appends amenity ids to the attribute """
+            if isinstance(obj, Amenity) and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
